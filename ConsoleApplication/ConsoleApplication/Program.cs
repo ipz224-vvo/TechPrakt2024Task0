05386641ef0b4d6efe49924f27ae7c3aee5326df
ipz224-vvo <ipz224_vvo@student.ztu.edu.ua>
2024-01-30 22:14:56 +0200

    Added gitignore, C# project

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApplication;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 2 numbers, delimeted by Enter:");
+        double firstNumber, secondNumber;
+        string firstNumberInput  = Console.ReadLine();
+
+        if(!double.TryParse(firstNumberInput, out firstNumber))
+        {
+            Console.WriteLine("First number is not valid!");
+            return;
+        }
+        string secondNumberInput = Console.ReadLine();
+        if (!double.TryParse(secondNumberInput, out secondNumber))
+        {
+            Console.WriteLine("Second number is not valid!");
+            return;
+        }
+        Console.WriteLine($"Sum of numbers: {firstNumber + secondNumber}");
+    }
+}
